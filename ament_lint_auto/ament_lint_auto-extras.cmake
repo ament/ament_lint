c504a7b3a0e6f04b5a07b413d1431e9ea7c52d3a
Dirk Thomas <dthomas@osrfoundation.org>
2015-08-21 11:37:43 -0700

    ensure that ament_package() was called before finding ament_lint_auto

--- ament_lint_auto/ament_lint_auto-extras.cmake
+++ ament_lint_auto/ament_lint_auto-extras.cmake
@@ -14,6 +14,10 @@
 
 # copied from ament_lint_auto/ament_lint_auto-extras.cmake
 
+if(_${PROJECT_NAME}_AMENT_PACKAGE)
+  message(FATAL_ERROR "find_package(ament_lint_auto) must be called before ament_package()")
+endif()
+
 find_package(ament_cmake_core QUIET REQUIRED)
 find_package(ament_cmake_test QUIET REQUIRED)
 
