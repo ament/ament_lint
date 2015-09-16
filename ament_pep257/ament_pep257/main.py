01c043bb56f2faa301c83d36ef8ef58ab161858c
Dirk Thomas <dthomas@osrfoundation.org>
2015-09-16 13:48:24 -0700

    add ament_pep257 package

---
+++ ament_pep257/ament_pep257/main.py
@@ -0,0 +1,204 @@
+#!/usr/bin/env python3
+
+# Copyright 2015 Open Source Robotics Foundation, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import print_function
+
+import argparse
+import logging
+import os
+import sys
+import time
+from xml.sax.saxutils import escape
+from xml.sax.saxutils import quoteattr
+
+from ament_pep257.pep257 import check
+from ament_pep257.pep257 import collect
+from ament_pep257.pep257 import Error
+from ament_pep257.pep257 import log
+log.setLevel(logging.INFO)
+
+
+def main(argv=sys.argv[1:]):
+    parser = argparse.ArgumentParser(
+        description='Check docstrings against the style conventions in PEP 257.',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument(
+        '--ignore',
+        nargs='*',
+        default=['D100', 'D101', 'D102', 'D103'],
+        help='The pep257 categories to ignore')
+    parser.add_argument(
+        'paths',
+        nargs='*',
+        default=[os.curdir],
+        help="The files or directories to check. For directories files ending "
+             "in '.py' will be considered.")
+    parser.add_argument(
+        '--exclude',
+        metavar='filename',
+        nargs='*',
+        default=[],
+        dest='excludes',
+        help='The filenames to exclude.')
+    # not using a file handle directly
+    # in order to prevent leaving an empty file when something fails early
+    parser.add_argument(
+        '--xunit-file',
+        help='Generate a xunit compliant XML file')
+    args = parser.parse_args(argv)
+
+    if args.xunit_file:
+        start_time = time.time()
+
+    excludes = [os.path.abspath(e) for e in args.excludes]
+    report = generate_pep257_report(args.paths, excludes, args.ignore)
+    error_count = sum([len(r[1]) for r in report])
+
+    # print summary
+    if not error_count:
+        print('No errors')
+        rc = 0
+    else:
+        print('%d errors' % error_count)
+        rc = 1
+
+    # generate xunit file
+    if args.xunit_file:
+        folder_name = os.path.basename(os.path.dirname(args.xunit_file))
+        file_name = os.path.basename(args.xunit_file)
+        suffix = '.xml'
+        if file_name.endswith(suffix):
+            file_name = file_name[0:-len(suffix)]
+        testname = '%s.%s' % (folder_name, file_name)
+
+        xml = get_xunit_content(report, testname, time.time() - start_time)
+        path = os.path.dirname(os.path.abspath(args.xunit_file))
+        if not os.path.exists(path):
+            os.makedirs(path)
+        with open(args.xunit_file, 'w') as f:
+            f.write(xml)
+
+    return rc
+
+
+def generate_pep257_report(paths, excludes, ignore):
+    def match(filename):
+        return filename.endswith('.py')
+
+    def match_dir(dirname):
+        return dirname[0] not in ['.', '_']
+
+    files = collect(paths, match=match, match_dir=match_dir)
+    files = [f for f in files if os.path.abspath(f) not in excludes]
+
+    report = []
+
+    for filename in sorted(files):
+        print('checking', filename)
+        errors = []
+        pep257_errors = check([filename], ignore=ignore)
+        for pep257_error in pep257_errors:
+            if isinstance(pep257_error, Error):
+                errors.append({
+                    'category': pep257_error.code,
+                    'linenumber': pep257_error.line,
+                    'message': pep257_error.message,
+                })
+                print(
+                    '%s:%d %s: %s' %
+                    (pep257_error.filename, pep257_error.line, pep257_error.definition,
+                     pep257_error.message))
+            elif isinstance(pep257_error, SyntaxError):
+                errors.append({
+                    'category': str(type(pep257_error)),
+                    'linenumber': '-',
+                    'message': 'invalid syntax in file',
+                })
+                print('%s: invalid syntax' % filename)
+            else:
+                errors.append({
+                    'category': 'unknown',
+                    'linenumber': '-',
+                    'message': str(pep257_error),
+                })
+                print('%s: %s' % (filename, pep257_error))
+        report.append((filename, errors))
+    return report
+
+
+def get_xunit_content(report, testname, elapsed):
+    test_count = sum([max(len(r[1]), 1) for r in report])
+    error_count = sum([len(r[1]) for r in report])
+    data = {
+        'testname': testname,
+        'test_count': test_count,
+        'error_count': error_count,
+        'time': '%.3f' % round(elapsed, 3),
+    }
+    xml = '''<?xml version="1.0" encoding="UTF-8"?>
+<testsuite
+  name="%(testname)s"
+  tests="%(test_count)d"
+  failures="%(error_count)d"
+  time="%(time)s"
+>
+''' % data
+
+    for (filename, errors) in report:
+
+        if errors:
+            # report each error as a failing testcase
+            for error in errors:
+                data = {
+                    'quoted_location': quoteattr(
+                        '%s (%s:%d)' % (
+                            error['category'], filename, error['linenumber'])),
+                    'testname': testname,
+                    'quoted_message': quoteattr(error['message']),
+                }
+                xml += '''  <testcase
+    name=%(quoted_location)s
+    classname="%(testname)s"
+  >
+      <failure message=%(quoted_message)s/>
+  </testcase>
+''' % data
+
+        else:
+            # if there are no lint_cmake errors report a single successful test
+            data = {
+                'quoted_location': quoteattr(filename),
+                'testname': testname,
+            }
+            xml += '''  <testcase
+    name=%(quoted_location)s
+    classname="%(testname)s"
+    status="No errors"/>
+''' % data
+
+    # output list of checked files
+    data = {
+        'escaped_files': escape(''.join(['\n* %s' % r[0] for r in report])),
+    }
+    xml += '''  <system-out>Checked files:%(escaped_files)s</system-out>
+''' % data
+
+    xml += '</testsuite>\n'
+    return xml
+
+
+if __name__ == '__main__':
+    sys.exit(main())
---
+++ ament_pep257/setup.py
@@ -0,0 +1,36 @@
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    name='ament_pep257',
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    install_requires=['setuptools'],
+    package_data={'': [
+        'configuration/ament_pep257.ini',
+    ]},
+    author='Dirk Thomas',
+    author_email='dthomas@osrfoundation.org',
+    maintainer='Dirk Thomas',
+    maintainer_email='dthomas@osrfoundation.org',
+    url='https://github.com/ament/ament_lint',
+    download_url='https://github.com/ament/ament_lint/releases',
+    keywords=['ROS'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Topic :: Software Development',
+    ],
+    description='Check Python code style using pep257.',
+    long_description='''\
+The ability to check code against the docstring conventions in PEP 257
+and generate xUnit test result files.''',
+    license='Apache License, Version 2.0',
+    test_suite='test',
+    entry_points={
+        'console_scripts': [
+            'ament_pep257 = ament_pep257.main:main',
+        ],
+    },
+)
