dae3cbfc85a6858b4a5dcbd065f4931e5b095f83
Dirk Thomas <dirk-thomas@users.noreply.github.com>
2018-07-09 11:55:04 -0700

    add ament_(cmake_)xmllint packages

---
+++ ament_xmllint/setup.py
@@ -0,0 +1,33 @@
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    name='ament_xmllint',
+    version='0.5.2',
+    packages=find_packages(exclude=['test']),
+    install_requires=['setuptools'],
+    author='Dirk Thomas',
+    author_email='dthomas@osrfoundation.org',
+    maintainer='Dirk Thomas',
+    maintainer_email='dthomas@osrfoundation.org',
+    url='https://github.com/ament/ament_lint',
+    download_url='https://github.com/ament/ament_lint/releases',
+    keywords=['ROS'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python',
+        'Topic :: Software Development',
+    ],
+    description='Check XML markup using xmllint.',
+    long_description="""\
+The ability to check XML files like the package manifest using xmllint
+and generate xUnit test result files.""",
+    license='Apache License, Version 2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'ament_xmllint = ament_xmllint.main:main',
+        ],
+    },
+)
