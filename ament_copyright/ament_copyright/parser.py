9f044f7662d94f6e32ff3c397a65682de6e029f6
jpsamper2009 <19418592+jpsamper2009@users.noreply.github.com>
2018-09-12 16:03:26 -0700

    Extend copyright checker to allow for doxygen-style copyright (#108)

--- ament_copyright/ament_copyright/parser.py
+++ ament_copyright/ament_copyright/parser.py
@@ -175,7 +175,8 @@ def search_copyright_information(content):
     year = '\d{4}'
     year_range = '%s-%s' % (year, year)
     year_or_year_range = '(?:%s|%s)' % (year, year_range)
-    pattern = '^[^\n\r]?\s*Copyright(?:\s+\(c\))?\s+(%s(?:,\s*%s)*),?\s+([^\n\r]+)$' % \
+    pattern = '^[^\n\r]?\s*(?:\\\copyright\s*)?' \
+              'Copyright(?:\s+\(c\))?\s+(%s(?:,\s*%s)*),?\s+([^\n\r]+)$' % \
         (year_or_year_range, year_or_year_range)
     regex = re.compile(pattern, re.DOTALL | re.MULTILINE)
 
@@ -239,7 +240,8 @@ def is_shebang_line(content, index):
 
 def get_comment_block(content, index):
     # regex for matching the beginning of the first comment
-    pattern = '^(#|//)'
+    # check for doxygen comments (///) before regular comments (//)
+    pattern = '^(#|///|//)'
     regex = re.compile(pattern, re.MULTILINE)
 
     match = regex.search(content, index)
