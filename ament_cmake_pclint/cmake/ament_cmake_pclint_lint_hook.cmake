26ee67d768694dffb57be1b130512cebd6682da8
Juan Pablo Samper <jp.samper@apex.ai>
2018-10-31 11:55:32 -0700

    Improvement to ament_cmake_pclint

--- ament_cmake_pclint/cmake/ament_cmake_pclint_lint_hook.cmake
+++ ament_cmake_pclint/cmake/ament_cmake_pclint_lint_hook.cmake
@@ -1,5 +1,5 @@
 # Copyright 2015 Open Source Robotics Foundation, Inc.
-# Copyright 2017 Apex.AI, Inc.
+# Copyright 2017-2018 Apex.AI, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
