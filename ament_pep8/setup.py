1f0961dc3273643d1dd48cb3084606918bc0c550
Steven! Ragnarök <steven@nuclearsandwich.com>
2018-12-06 18:58:30 -0800

    0.6.2

--- ament_pep8/setup.py
+++ ament_pep8/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='ament_pep8',
-    version='0.6.1',
+    version='0.6.2',
     packages=find_packages(exclude=['test']),
     install_requires=['setuptools'],
     package_data={'': [
