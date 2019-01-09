ba7108b5f05709c511f26ed5d5778b04aef8d18b
Jacob Perron <jacob@openrobotics.org>
2019-01-09 14:32:03 -0800

    Add find_package and dependency tag for ament_cmake_core

--- ament_cmake_cppcheck/cmake/ament_cmake_cppcheck_lint_hook.cmake
+++ ament_cmake_cppcheck/cmake/ament_cmake_cppcheck_lint_hook.cmake
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+find_package(ament_cmake_core REQUIRED)
+
 file(GLOB_RECURSE _source_files FOLLOW_SYMLINKS
   "*.c"
   "*.cc"
