63fc82d7249fd655c5313a83c82380a49f250b99
Jacob Perron <jacob@openrobotics.org>
2019-01-10 16:53:41 -0800

    Account for INTERFACE libraries when getting target include directories
    CMake does not allow getting the INCLUDE_DIRECTORIES property from
    INTERFACE libraries.
    Instead, first check if the property exists, if it does not then try to
    get the INTERFACE_INCLUDE_DIRECTORIES property.
    
    Note, if INTERFACE_INCLUDE_DIRECTORIES is not defined an empty list is
    returned, but we cannot assume the target is not an interface.
    This is why the implementation is conditional on INCLUDE_DIRECTORIES
    instead.
    
    Signed-off-by: Jacob Perron <jacob@openrobotics.org>

--- ament_cmake_cppcheck/cmake/ament_cmake_cppcheck_lint_hook.cmake
+++ ament_cmake_cppcheck/cmake/ament_cmake_cppcheck_lint_hook.cmake
@@ -33,18 +33,38 @@ if(_source_files)
   if(NOT CMAKE_VERSION VERSION_LESS "3.7.0")
     get_directory_property(_build_targets DIRECTORY ${CMAKE_SOURCE_DIR} BUILDSYSTEM_TARGETS)
     foreach(_target ${_build_targets})
-      get_property(_include_dirs
+      # Check if INCLUDE_DIRECTORIES is defined first in case the target is an INTERFACE
+      get_property(_include_dirs_defined
         TARGET ${_target}
         PROPERTY INCLUDE_DIRECTORIES
+        DEFINED
       )
+
+      if(_include_dirs_defined)
+        get_property(_include_dirs
+          TARGET ${_target}
+          PROPERTY INCLUDE_DIRECTORIES
+        )
+      else()
+        # Target might be an interface
+        get_property(_include_dirs
+          TARGET ${_target}
+          PROPERTY INTERFACE_INCLUDE_DIRECTORIES
+        )
+      endif()
 
       # Only append include directories that are from the package being tested
       # This accomplishes two things:
       #     1. Reduces execution time (less include directories to search)
       #     2. cppcheck will not check for errors in external packages
       foreach(_include_dir ${_include_dirs})
-        string(REGEX MATCH "^${CMAKE_SOURCE_DIR}.*" _is_match ${_include_dir})
-        if(_is_match)
+        # TODO(jacobperron): Escape special regex characters in CMAKE_SOURCE_DIR
+        #                    Related CMake feature request: https://gitlab.kitware.com/cmake/cmake/issues/18409
+        # Check if include directory is a subdirectory of the source directory
+        string(REGEX MATCH "^${CMAKE_SOURCE_DIR}/.*" _is_subdirectory ${_include_dir})
+        # Check if include directory is part of a generator expression (e.g. $<BUILD_INTERFACE:...>)
+        string(REGEX MATCH "^\\$<.*:${CMAKE_SOURCE_DIR}/.*>$" _is_genexp_subdirectory "${_include_dir}")
+        if(_is_subdirectory OR _is_genexp_subdirectory)
           list_append_unique(_all_include_dirs ${_include_dir})
         endif()
       endforeach()
