440d4560d39cb4f0880722ad26b968ea6e41cf0e
Joshua Whitley <jwhitley@autonomoustuff.com>
2019-02-19 22:03:04 -0600

    Adding LGPL to ament_copyright.
    Signed-off-by: Joshua Whitley <jwhitley@autonomoustuff.com>

--- ament_copyright/setup.py
+++ ament_copyright/setup.py
@@ -37,6 +37,7 @@ The ability to check sources file for copyright and license information.""",
             'bsd2 = ament_copyright.licenses:bsd2',
             'mit = ament_copyright.licenses:mit',
             'gplv3 = ament_copyright.licenses:gplv3',
+            'lgplv3 = ament_copyright.licenses:lgplv3',
         ],
         'console_scripts': [
             'ament_copyright = ament_copyright.main:main',
