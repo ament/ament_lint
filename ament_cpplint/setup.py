91132c83ff22c14bbe7d031e573804eeb922d3e5
Steven! Ragnarök <steven@nuclearsandwich.com>
2019-04-11 07:23:12 -0700

    0.7.0

--- ament_cpplint/setup.py
+++ ament_cpplint/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='ament_cpplint',
-    version='0.6.3',
+    version='0.7.0',
     packages=find_packages(exclude=['test']),
     install_requires=['setuptools'],
     zip_safe=True,
