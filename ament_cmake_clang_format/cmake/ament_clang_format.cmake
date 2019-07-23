3ba63654265131d881e67148c864371bf48105b5
John <johnshepherd96@yahoo.com>
2019-07-23 16:35:18 -0700

    Add custom config file support for clang-format (#153)
    * Add custom config file support for clang-format
    
    Signed-off-by: John Shepherd <johnshepherd96@yahoo.com>
    
    * Add global arg into cmake function, update documentation
    
    Signed-off-by: John Shepherd <johnshepherd96@yahoo.com>

--- ament_cmake_clang_format/cmake/ament_clang_format.cmake
+++ ament_cmake_clang_format/cmake/ament_clang_format.cmake
@@ -15,15 +15,25 @@
 #
 # Add a test to check the code for compliance with clang_format.
 #
+# The default configuration file used for clang-format is located at 
+# configuration/.clang-format within the ament_clang_format directory
+# The default configuration file can be either overridden by the
+# argument 'CONFIG_FILE' or by a global variable named
+# 'ament_cmake_clang_format_CONFIG_FILE'
+# The 'CONFIG_FILE' argument takes priority over 
+# 'ament_cmake_clang_format_CONFIG_FILE' if both are defined
+# 
 # :param TESTNAME: the name of the test, default: "clang_format"
 # :type TESTNAME: string
+# :param CONFIG_FILE: the path of the configuration file for clang-format to consider
+# :type CONFIG_FILE: string
 # :param ARGN: the files or directories to check
 # :type ARGN: list of strings
 #
 # @public
 #
 function(ament_clang_format)
-  cmake_parse_arguments(ARG "" "TESTNAME" "" ${ARGN})
+  cmake_parse_arguments(ARG "" "TESTNAME;CONFIG_FILE" "" ${ARGN})
   if(NOT ARG_TESTNAME)
     set(ARG_TESTNAME "clang_format")
   endif()
@@ -36,7 +46,12 @@ function(ament_clang_format)
   set(result_file "${AMENT_TEST_RESULTS_DIR}/${PROJECT_NAME}/${ARG_TESTNAME}.xunit.xml")
   set(cmd "${ament_clang_format_BIN}" "--xunit-file" "${result_file}")
   list(APPEND cmd ${ARG_UNPARSED_ARGUMENTS})
-
+  if(ARG_CONFIG_FILE)
+    set(APPEND cmd "--config" "${ARG_CONFIG_FILE}")
+  elseif(DEFINED ament_cmake_clang_format_CONFIG_FILE)
+    set(APPEND cmd "--config" "${ament_cmake_clang_format_CONFIG_FILE}")
+  endif()
+  
   file(MAKE_DIRECTORY "${CMAKE_BINARY_DIR}/ament_clang_format")
   ament_add_test(
     "${ARG_TESTNAME}"
