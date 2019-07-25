f088f5fb062c9a125a868116a160f3cc86f71bfc
Ted Kern <ted.kern@canonical.com>
2019-07-25 17:34:18 +0000

    readd copied from comment
    Signed-off-by: Ted Kern <ted.kern@canonical.com>

--- ament_cmake_mypy/cmake/ament_cmake_mypy_lint_hook.cmake
+++ ament_cmake_mypy/cmake/ament_cmake_mypy_lint_hook.cmake
@@ -11,6 +11,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
+# copied from ament_cmake_mypy/ament_cmake_mypy-extras.cmake
 
 file(GLOB_RECURSE _python_files FOLLOW_SYMLINKS "*.py")
 if(_python_files)
