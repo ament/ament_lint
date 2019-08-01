2a95994262843731c8317c12d0292f57fc0194c6
Steven! Ragnarök <steven@nuclearsandwich.com>
2019-08-01 04:46:07 -0700

    0.7.5

--- ament_flake8/setup.py
+++ ament_flake8/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='ament_flake8',
-    version='0.7.4',
+    version='0.7.5',
     packages=find_packages(exclude=['test']),
     install_requires=['setuptools'],
     package_data={'': [
--- ament_pclint/setup.py
+++ ament_pclint/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='ament_pclint',
-    version='0.7.4',
+    version='0.7.5',
     packages=find_packages(exclude=['test']),
     install_requires=['setuptools'],
     package_data={'': [
