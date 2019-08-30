c6637fa9b06d4db1c7d761ca6bb07626ef9c5715
Dirk Thomas <dirk-thomas@users.noreply.github.com>
2019-08-30 11:36:53 -0700

    install manifest files in Python packages
    Signed-off-by: Dirk Thomas <dirk-thomas@users.noreply.github.com>

--- ament_cpplint/setup.py
+++ ament_cpplint/setup.py
@@ -1,10 +1,15 @@
 from setuptools import find_packages
 from setuptools import setup
 
+package_name = 'ament_cpplint'
+
 setup(
-    name='ament_cpplint',
+    name=package_name,
     version='0.7.4',
     packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/' + package_name, ['package.xml']),
+    ],
     install_requires=['setuptools'],
     zip_safe=True,
     author='Dirk Thomas',
--- ament_mypy/setup.py
+++ ament_mypy/setup.py
@@ -1,10 +1,15 @@
 from setuptools import find_packages
 from setuptools import setup
 
+package_name = 'ament_mypy'
+
 setup(
-    name='ament_mypy',
+    name=package_name,
     version='0.7.3',
     packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/' + package_name, ['package.xml']),
+    ],
     install_requires=['setuptools'],
     package_data={'': [
         'configuration/ament_mypy.ini',
