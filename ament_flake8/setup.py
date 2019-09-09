5aa7259f79796466cb59af8660d0336ab48cc7d9
Scott K Logan <logans@cottsay.net>
2019-09-09 12:23:28 -0700

    Install manifest file in ament_flake8
    Follow-up to #186
    Backport of #189
    
    Signed-off-by: Scott K Logan <logans@cottsay.net>

--- ament_flake8/setup.py
+++ ament_flake8/setup.py
@@ -1,10 +1,15 @@
 from setuptools import find_packages
 from setuptools import setup
 
+package_name = 'ament_flake8'
+
 setup(
-    name='ament_flake8',
+    name=package_name,
     version='0.7.9',
     packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/' + package_name, ['package.xml']),
+    ],
     install_requires=['setuptools'],
     package_data={'': [
         'configuration/ament_flake8.ini',
