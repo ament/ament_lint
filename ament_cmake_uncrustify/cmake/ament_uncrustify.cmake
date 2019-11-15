222106f68f43f4b27b046c5b5ec8bda8e6600315
Yadunund <yadunund@openrobotics.org>
2019-11-15 09:53:55 +0800

    reverted deletion of empty line after copyright header
    Signed-off-by: Yadunund <yadunund@openrobotics.org>

--- ament_cmake_uncrustify/cmake/ament_uncrustify.cmake
+++ ament_cmake_uncrustify/cmake/ament_uncrustify.cmake
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 #
 # Add a test to check the code for compliance with uncrustify.
 #
