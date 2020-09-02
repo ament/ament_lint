3f8fcb38e40dafcf2c9078ff948801b46125a301
Dan Rose <dan@digilabs.io>
2020-09-01 23:48:22 -0500

    more

--- ament_cmake_cppcheck/cmake/ament_cmake_cppcheck_lint_hook.cmake
+++ ament_cmake_cppcheck/cmake/ament_cmake_cppcheck_lint_hook.cmake
@@ -27,12 +27,6 @@ file(GLOB_RECURSE _source_files FOLLOW_SYMLINKS
 if(_source_files)
   message(STATUS "Added test 'cppcheck' to perform static code analysis on C / C++ code")
 
-  # Get include paths for added targets
-  set(_all_include_dirs "")
-  if(DEFINED ament_cmake_cppcheck_ADDITIONAL_INCLUDE_DIRS)
-    list(APPEND _all_include_dirs ${ament_cmake_cppcheck_ADDITIONAL_INCLUDE_DIRS})
-  endif()
-
   # Forces cppcheck to consider ament_cmake_cppcheck_LANGUAGE as the given language if defined
   set(_language "")
   if(DEFINED ament_cmake_cppcheck_LANGUAGE)
@@ -40,41 +34,24 @@ if(_source_files)
     message(STATUS "Configured cppcheck language: ${ament_cmake_cppcheck_LANGUAGE}")
   endif()
 
-  # Get exclude paths for added targets
-  set(_all_exclude "")
-  if(DEFINED ament_cmake_cppcheck_ADDITIONAL_EXCLUDE)
-    list(APPEND _all_exclude ${ament_cmake_cppcheck_ADDITIONAL_EXCLUDE})
-  endif()
-
-  set(_all_definitions "")
-  
   get_directory_property(_build_targets DIRECTORY ${PROJECT_SOURCE_DIR} BUILDSYSTEM_TARGETS)
   foreach(_target ${_build_targets})
     # Include directories property is different for INTERFACE libraries
     get_target_property(_target_type ${_target} TYPE)
     if(${_target_type} STREQUAL "INTERFACE_LIBRARY")
-      get_target_property(_include_dirs ${_target} INTERFACE_INCLUDE_DIRECTORIES)
-      get_target_property(_definitions ${_target} INTERFACE_COMPILE_DEFINITIONS)
+      set(_include_dirs $<TARGET_PROPERTY:${_target},INTERFACE_INCLUDE_DIRECTORIES>)
+      set(_definitions $<TARGET_PROPERTY:${_target},INTERFACE_COMPILE_DEFINITIONS>)
     else()
-      get_target_property(_include_dirs ${_target} INCLUDE_DIRECTORIES)
-      get_target_property(_definitions ${_target} COMPILE_DEFINITIONS)
+      set(_include_dirs $<TARGET_PROPERTY:${_target},INCLUDE_DIRECTORIES>)
+      set(_definitions $<TARGET_PROPERTY:${_target},COMPILE_DEFINITIONS>)
     endif()
 
-    list(APPEND _all_include_dirs ${_include_dirs})
-    list(APPEND _all_definitions ${_definitions})
+    ament_cppcheck(
+      TESTNAME ${target}_ament_cppcheck
+      ${_language}
+      INCLUDE_DIRS ${ament_cmake_cppcheck_ADDITIONAL_INCLUDE_DIRS} ${_include_dirs}
+      EXCLUDE  ${ament_cmake_cppcheck_ADDITIONAL_EXCLUDE}
+      DEFINITIONS ${_definitions}
+    )
   endforeach()
-
-  message(STATUS "Configured cppcheck include dirs: ${_all_include_dirs}")
-  message(
-    STATUS "Configured cppcheck exclude dirs and/or files: ${_all_exclude}"
-  )
-  message(
-    STATUS "Configured cppcheck definitions: ${_all_definitions}"
-  )
-  ament_cppcheck(
-    ${_language}
-    INCLUDE_DIRS ${_all_include_dirs}
-    EXCLUDE ${_all_exclude}
-    DEFINITIONS ${_all_definitions}
-  )
 endif()
