cbed0b9ef48f55bb3fa65f4ad666684dbc493c80
Abrar Rahman Protyasha <aprotyas@u.rochester.edu>
2021-10-20 09:10:21 -0400

    Add test cases to check exclusion behavior
    Specifically, these tests check for:
    
    - Incorrect exclusion of single filenames.
    - Correct exclusion of relatively/absolutely addressed filenames.
    - Correct exclusion of wildcarded paths.
    
    Signed-off-by: Abrar Rahman Protyasha <aprotyas@u.rochester.edu>

--- ament_copyright/test/test_copyright.py
+++ ament_copyright/test/test_copyright.py
@@ -12,12 +12,15 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from contextlib import redirect_stdout
+import io
 import os
 
 from ament_copyright.main import main
 
 
 cases_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'cases')
+f = io.StringIO()
 
 
 def test_apache2_standard():
@@ -72,4 +75,48 @@ def test_mit_py():
 
 def test_mit_cpp():
     rc = main(argv=[os.path.join(cases_path, 'mit/case2.cpp')])
+    assert rc == 0, 'Found errors'
+
+
+def test_incorrect_exclusion():
+    """
+    Checks that excluding a single filename does not work.
+
+    `ament_copyright <path> --exclude <file-name>` should not exclude anything.
+    """
+    rc = main(argv=[os.path.join(cases_path, 'mit/case.py'), '--exclude', 'case.py'])
+    assert rc == 0, 'Found errors'
+
+
+def test_correct_exclusion():
+    """
+    Checks that excluding a file relatively/absolutely works as expected.
+
+    `ament_copyright <path/filename> --exclude <path/filename>` should exclude <path/filename>.
+    """
+    with redirect_stdout(f):
+        rc = main(
+            argv=[
+                os.path.join(cases_path, 'mit/case.py'),
+                '--exclude',
+                os.path.join(cases_path, 'mit/case.py')
+            ])
+    assert 'No repository roots and files found' in f.getvalue()
+    assert rc == 0, 'Found errors'
+
+
+def test_wildcard_exclusion():
+    """
+    Checks that wildcard exclusion works correctly.
+
+    `ament_copyright <path> --exclude <path/*>` should exclude all files in <path>.
+    """
+    with redirect_stdout(f):
+        rc = main(
+            argv=[
+                os.path.join(cases_path, 'mit'),
+                '--exclude',
+                os.path.join(cases_path, 'mit/*')
+            ])
+    assert 'No repository roots and files found' in f.getvalue()
     assert rc == 0, 'Found errors'
