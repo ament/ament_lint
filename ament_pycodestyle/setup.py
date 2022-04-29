5fae9a37391d6052213d40d40147421ea3fe3f38
Audrow Nash <audrow@hey.com>
2022-04-29 16:17:48 -0700

    0.13.0
    Signed-off-by: Audrow Nash <audrow@hey.com>

--- ament_pycodestyle/setup.py
+++ ament_pycodestyle/setup.py
@@ -5,7 +5,7 @@ package_name = 'ament_pycodestyle'
 
 setup(
     name=package_name,
-    version='0.12.3',
+    version='0.13.0',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/' + package_name, ['package.xml']),
