df570848f8c5f72f0e3f238359ff4285ce98094f
William Woodall <william@osrfoundation.org>
2022-05-04 16:18:34 -0700

    an improved message and avoiding missing new lines between reports from files (#373)
    Signed-off-by: William Woodall <william@osrfoundation.org>

--- ament_clang_tidy/ament_clang_tidy/main.py
+++ ament_clang_tidy/ament_clang_tidy/main.py
@@ -165,8 +165,10 @@ def main(argv=sys.argv[1:]):
         def start_subprocess(full_cmd):
             output = ''
             try:
-                output = subprocess.check_output(full_cmd,
-                                                 stderr=subprocess.DEVNULL).strip().decode()
+                output = subprocess.check_output(
+                    full_cmd,
+                    stderr=subprocess.DEVNULL
+                ).decode()
             except subprocess.CalledProcessError as e:
                 print('The invocation of "%s" failed with error code %d: %s' %
                       (os.path.basename(clang_tidy_bin), e.returncode, e),
@@ -201,7 +203,7 @@ def main(argv=sys.argv[1:]):
     for compilation_db in compilation_dbs:
         package_dir = os.path.dirname(compilation_db)
         package_name = os.path.basename(package_dir)
-        print('found compilation database for package "%s"...' % package_name)
+        print(f"found compilation database for package '{package_name}' at '{compilation_db}'")
         (source_files, output) = invoke_clang_tidy(compilation_db)
         files += source_files
         outputs.append(output)
