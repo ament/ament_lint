07a8039f540789abfe2b66655a0348816cc2aafd
Cristóbal Arroyo <69475004+Crola1702@users.noreply.github.com>
2023-01-03 16:57:45 -0500

    Added underscore to ignore new pydocstyle item (#428)
    * Added underscore to ignore new item
    
    Signed-off-by: Crola1702 <cristobal.arroyo@ekumenlabs.com>
    (cherry picked from commit ae431edfeb2d1fb7e92c37c67bce9c4237e9ac43)
    Signed-off-by: Audrow Nash <audrow@openrobotics.org>

--- ament_pep257/ament_pep257/main.py
+++ ament_pep257/ament_pep257/main.py
@@ -187,7 +187,8 @@ def generate_pep257_report(paths, excludes, ignore, select, convention, add_igno
     report = []
 
     files_dict = {}
-    for filename, checked_codes, ignore_decorators in files_to_check:
+    # Unpack 3 values for pydocstyle <= 6.1.1 and 4 values for pydocstyle >= 6.2.0
+    for filename, checked_codes, ignore_decorators, *_ in files_to_check:
         if _filename_in_excludes(filename, excludes):
             continue
         files_dict[filename] = {
