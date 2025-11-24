fe918dab6674d029a807b9681b7b80cfc86581eb
Michael Carlstrom <rmc@carlstrom.com>
2025-11-24 09:19:35 -0800

    Remove importlib_metadata
    Signed-off-by: Michael Carlstrom <rmc@carlstrom.com>

--- ament_copyright/ament_copyright/__init__.py
+++ ament_copyright/ament_copyright/__init__.py
@@ -12,10 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-try:
-    import importlib.metadata as importlib_metadata
-except ModuleNotFoundError:
-    import importlib_metadata
+from importlib import metadata
 
 
 COPYRIGHT_GROUP = 'ament_copyright.copyright_name'
@@ -38,11 +35,8 @@ UNKNOWN_IDENTIFIER = '<unknown>'
 
 def get_copyright_names():
     names = {}
-    entry_points = importlib_metadata.entry_points()
-    if hasattr(entry_points, 'select'):
-        copyright_groups = entry_points.select(group=COPYRIGHT_GROUP)
-    else:
-        copyright_groups = entry_points.get(COPYRIGHT_GROUP, [])
+    entry_points = metadata.entry_points()
+    copyright_groups = entry_points.select(group=COPYRIGHT_GROUP)
     for entry_point in copyright_groups:
         assert entry_point.name != UNKNOWN_IDENTIFIER, \
             "Invalid entry point name '%s'" % entry_point.name
@@ -53,11 +47,8 @@ def get_copyright_names():
 
 def get_licenses():
     licenses = {}
-    entry_points = importlib_metadata.entry_points()
-    if hasattr(entry_points, 'select'):
-        license_groups = entry_points.select(group=LICENSE_GROUP)
-    else:
-        license_groups = entry_points.get(LICENSE_GROUP, [])
+    entry_points = metadata.entry_points()
+    license_groups = entry_points.select(group=LICENSE_GROUP)
     for entry_point in license_groups:
         assert entry_point.name != UNKNOWN_IDENTIFIER, \
             "Invalid entry point name '%s'" % entry_point.name
