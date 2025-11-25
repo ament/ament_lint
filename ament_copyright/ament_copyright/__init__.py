fd96230423ad068c1e24402fff554e55f2940f2d
Michael Carlstrom <rmc@carlstrom.com>
2025-11-25 08:11:33 -0800

    fix flake8
    Signed-off-by: Michael Carlstrom <rmc@carlstrom.com>

--- ament_copyright/ament_copyright/__init__.py
+++ ament_copyright/ament_copyright/__init__.py
@@ -12,8 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import sys
 from importlib import metadata
+import sys
 
 
 COPYRIGHT_GROUP = 'ament_copyright.copyright_name'
