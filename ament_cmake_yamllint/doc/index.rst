fec77d653c7764fa38f8a82b9bae209c93b4028b
wj <frozenreboot@gmail.com>
2026-01-20 22:29:22 +0900

    Refactor: Apply review comments (Update version to 0.20.3, Copyright 2026)
    Signed-off-by: wj <frozenreboot@gmail.com>

--- ament_cmake_yamllint/doc/index.rst
+++ ament_cmake_yamllint/doc/index.rst
@@ -2,15 +2,13 @@ ament_yamllint
 ==============
 
 Checks YAML files using `yamllint <https://yamllint.readthedocs.io>`_.
+
 Files with the following extensions are being considered: ``.yaml``, ``.yml``.
 
-
 How to run the check from the command line?
--------------------------------------------
 
 The command line tool is provided by the package `ament_yamllint
 <https://github.com/ament/ament_lint>`_.
-
 
 How to run the check from within a CMake ament package as part of the tests?
 ----------------------------------------------------------------------------
--- ament_yamllint/setup.py
+++ ament_yamllint/setup.py
@@ -6,7 +6,7 @@ package_name = 'ament_yamllint'
 
 setup(
     name=package_name,
-    version='0.11.4',
+    version='0.20.3',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/' + package_name, ['package.xml']),
@@ -18,16 +18,15 @@ setup(
         'configuration/yamllint.yaml',
     ]},
     zip_safe=False,
-    author='Scott K Logan',
-    author_email='logans@cottsay.net',
-    maintainer='Michael Jeronimo, Michel Hidalgo',
-    maintainer_email='michael.jeronimo@openrobotics.org, michel@ekumenlabs.com',
+    author='Woojin Jung',
+    author_email='frozenreboot@gmail.com',
+    maintainer='Scott K Logan, Alejandro Hernández Cordero',
+    maintainer_email='logans@cottsay.net, ahcorde@gmail.com',
     url='https://github.com/ament/ament_lint',
     download_url='https://github.com/ament/ament_lint/releases',
     keywords=['ROS'],
     classifiers=[
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python',
         'Topic :: Software Development',
     ],
@@ -36,7 +35,11 @@ setup(
 The ability to check YAML against style conventions using YAMLlint
 and generate xUnit test result files.""",
     license='Apache License, Version 2.0',
-    tests_require=['pytest'],
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
     entry_points={
         'console_scripts': [
             'ament_yamllint = ament_yamllint.main:main',
