27b5740ec08f991476564f999e307ff526ef569f
Michael Carlstrom <rmc@carlstrom.com>
2026-03-01 14:11:46 -0800

    Fix path on pytest<7.0.0
    Signed-off-by: Michael Carlstrom <rmc@carlstrom.com>

--- ament_flake8/ament_flake8/pytest_marker.py
+++ ament_flake8/ament_flake8/pytest_marker.py
@@ -12,7 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from pytest import Config
+
+from _pytest.config import Config
 
 
 def pytest_configure(config: Config) -> None:
