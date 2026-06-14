13931bde1f193be40216d5925c088bd112d3d166
wj <frozenreboot@gmail.com>
2026-06-14 14:57:14 +0900

    Bump version to 0.21.1 to match rolling
    Signed-off-by: wj <frozenreboot@gmail.com>

--- ament_yamllint/setup.py
+++ ament_yamllint/setup.py
@@ -6,7 +6,7 @@ package_name = 'ament_yamllint'
 
 setup(
     name=package_name,
-    version='0.20.3',
+    version='0.21.1',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/' + package_name, ['package.xml']),
