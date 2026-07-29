9edb62c81daa1fcb6fa726fef4032ff1fcb4953f
Michael Carlstrom <rmc@carlstrom.com>
2026-07-28 19:27:45 -0700

    add ament_cmake_ruff
    Signed-off-by: Michael Carlstrom <rmc@carlstrom.com>

---
+++ ament_cmake_ruff/cmake/ament_cmake_ruff_lint_hook.cmake
@@ -0,0 +1,20 @@
+# Copyright 2026 Open Source Robotics Foundation, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+file(GLOB_RECURSE _python_files FOLLOW_SYMLINKS "*.py" "*.pyi")
+if(_python_files)
+  message(STATUS "Added test 'ruff' to check Python code syntax and style conventions")
+  message(STATUS "Configured 'ruff' exclude dirs and/or files: ${AMENT_LINT_AUTO_FILE_EXCLUDE}")
+  ament_ruff(EXCLUDE ${AMENT_LINT_AUTO_FILE_EXCLUDE})
+endif()
